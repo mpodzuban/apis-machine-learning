9e995c5d489d00f1d93f818868ab6d2064656b5a
mpodzuban <11062016>
2022-12-16 16:56:29 +0100

    json serializer update + requirements update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask_cors import CORS
+import json
 from prophet.serialize import model_from_json
 
 app = Flask(__name__)
@@ -12,14 +13,15 @@ with open('serialized_model.json', 'r') as fin:
 @app.route("/predict", methods=["GET"])
 def predict():
     scores = int(request.args.get('days'))
-    future_dates = my_model.make_future_dataframe(periods=7)
+    future_dates = my_model.make_future_dataframe(periods=1)
     forecast = my_model.predict(future_dates)
     fore_dict = forecast.tail(scores).to_dict('records')
     response = []
     for fore in fore_dict:
         response.append({key: fore[key]
                          for key in fore if key in ["ds", "trend", "yhat"]})
-    return response, 200
+        response[-1]["ds"] = str(response[-1]["ds"])
+    return json.dumps(response), 200
 
 
 if __name__ == "__main__":
